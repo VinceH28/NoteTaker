8d052f776157345b58f3efc2e2ada9852a721230
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-04-23 01:52:39 -0400

    Update apiroutes.js

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,62 +1,39 @@
 //REQUIRE fs TO WRITE TO FILE
-var fs = require("fs");
+const fs = require("fs");
+const path = require("path");
+let notes = require("../db/db.json");
+const { v4: uuidv4 } = require("uuid");
 
 //ROUTING
 module.exports = function (app) {
     //API GET Requests
     app.get("/api/notes", function (req, res) {
-        //The app has to read the database file located in ./db/db.json
-        fs.readFile("./db/db.json", "utf8", (err, data) => {
-            if (!data) {
-                return (data = []);
-            }
-            if (err) throw err;
-            let notes = JSON.parse(data);
             res.json(notes);
-        });
     });
 
     //API POST Requests
     app.post("/api/notes", function (req, res) {
-        fs.readFile("./db/db.json", "utf8", (err, data) => {
-        let notes = [];
-        if (err) throw err;
-    if (!data) {
-        console.log('No data!');
-        notes = [];
-    } else {
-        notes = JSON.parse(data);
-    }
-    console.log("These are the notes", notes);
-    const inputNote = {
-        id: Math.floor(Math.random() * 100),
-        title: req.body.title,
-        test: req.body.text,
-    };
-    notes.push(inputNote);
-    
-    console.log("The new note as been added!");
+            var newData = req.body;
+            newData.id = uuidv4();
 
-    fs.writeFileSync("./db/db.json", JSON.stringify(notes), "utf-8");
-    res.json(inputNote);
-     })
-    });
+            console.log("These are the notes", newData);
 
-    //Delete option
-    app.delete("/api/notes/:id", function (req, res) {
-        fs.readFile("./db/db.json", "utf8", (err, data) => {
-            if (err) throw err;
-            let notes = JSON.parse(data);
+            notes.push(newData);
 
-            const filteredNotes = notes.filter(note => {
-            return note.id != req.params.id
-            });
+            console.log("The new note as been added!");
 
-            console.log("Note deleted! The notes are now:", filteredNotes)
+            fs.writeFileSync(path.join(__dirname, "../db/db.json"), 
+            JSON.stringify(notes))
+            res.json(notes);
+          });
 
-            fs.writerFileSync("./db.db.json", JSON.stringify
-            (filteredNotes), "utf-8");
-            res.sendStatus(200);
-        });
-    });
-};
+         //API DELETE Requests
+         app.delete("/api/notes/:id", function (req, res) {
+            var noteIdDelete = req.params.id;
+            notes = notes.filter((note) => note.id != noteIdDelete);
+
+            console.log("Note deleted! The notes are now:", noteIdDelete)
+            
+            res.json(notes);
+          });
+        }; 
