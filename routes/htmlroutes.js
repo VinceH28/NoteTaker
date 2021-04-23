56d847d9dd8191c5d2803421052dc6d5ace64647
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-04-23 01:21:51 -0400

    Update htmlroutes.js

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,16 +1,17 @@
 // DEPENDENCIES
+const fs = require("fs");
 var path = require("path");
 
 // ROUTING
 
 module.exports = function (app) {
   // HTML GET Requests
-  app.get("/notes", function (req, res) {
+  app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
   
   //Default home when no matching route found
-  app.get("*", function (req, res) {
+  app.get("*", function (_req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });  
 };
